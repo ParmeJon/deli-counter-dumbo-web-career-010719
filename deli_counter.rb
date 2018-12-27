e50abcb9a337608ed37f29dffee2221c475b4470
Jonathan Chan <jonchan816@gmail.com>
2018-12-27 08:18:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,8 +18,9 @@ end
 
 def take_a_number(katz_deli, name)
   position = katz_deli.length + 1
-  puts "Welcome, #{name}. You are number #{position} in line."
-  katz_deli.push(name)
+  
+    puts "Welcome, #{name}. You are number #{position} in line."
+    katz_deli.push(name)
 end
 
 def now_serving
